0ce5d82b8b83d496ac98467be321019fc77120bb
Dingyi Zhao <dingyizhao.zdy@outlook.com>
2023-08-27 23:19:54 +0800

    fix: Remove unused peripheral initialize

--- board/Core/Src/main.c
+++ board/Core/Src/main.c
@@ -91,12 +91,7 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
-  MX_DMA_Init();
-  MX_USART6_UART_Init();
-  MX_SPI1_Init();
-  MX_TIM10_Init();
   MX_USB_DEVICE_Init();
-  MX_CAN1_Init();
   /* USER CODE BEGIN 2 */
   driver_main();
   /* USER CODE END 2 */
